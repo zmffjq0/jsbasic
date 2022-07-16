733cdb576f9f565be2742b92616a00ae25145beb
taeyun <zmffjq0@naver.com>
2022-07-16 15:08:08 +0900

    deepcopy update

--- deepcopyEx.js
+++ deepcopyEx.js
@@ -1,57 +1,59 @@
-const kim2 = [
+const kim2 = {
+  id: 3,
+  city: 'Pusan',
+  addr: { id: 1, name: 'Hong', addr: { city: 'Seoul', arr: [1, 2, 3] } },
+  ids: [{ id: 1 }, { id: 2 }],
+  fc: function () {
+    return True;
+  },
+};
+
+const kim3 = [
   3,
   'Pusan',
-  { id: 1, name: 'Hong', addr: { city: 'Seoul' } },
+  { id: 1, name: 'Hong', addr: { city: 'Seoul', arr: [1, 2, 3] } },
   [{ id: 1 }, { id: 2 }],
   function fc() {
     return True;
   },
 ];
 
-const newKim3 = deepCopyObject(kim2);
-newKim3[1] = 'Daegu';
-newKim3[2].name = 'Kim';
-newKim3[2].addr.city = 'Daejeon';
-newKim3[3][0].id = 3;
-newKim3[4] = 3;
-console.log(`kim2 = ${kim2}\n----------------------`);
-console.log(`newKim3 = ${newKim3}`);
-// console.log(`kim = ${kim2} newKim3 = ${newKim3}`);
-console.log(
-  kim2[1] !== newKim3[1],
-  kim2[2].name !== newKim3[2].name,
-  kim2[2].addr.city !== newKim3[2].addr.city,
-  kim2[3][0].id !== newKim3[3][0].id,
-  kim2[4] !== newKim3[4]
-);
+// *********** 배열로 시작할 때 ***************
+// const newKim3 = deepCopyObject(kim3);
+// newKim3[1] = 'Daegu';
+// newKim3[2].name = 'Kim';
+// newKim3[2].addr.city = 'Daejeon';
+// newKim3[3][0] = 3;
+// newKim3[2].addr.arr[0] = 100;
+// newKim3[4] = 'aaaa';
+// console.log('kim3 ===============>');
+// console.log(kim3);
+// console.log('newkim3 ===============>');
+// console.log(newKim3);
+
+// *********** 객체로 시작할 때 ***************
+const newKim2 = deepCopyObject(kim2);
+newKim2.ids[0].id = 100;
+// newKim2.fc = 'bbbb';
+console.log('----------------------');
+console.log(kim2);
+console.log('----------------------');
+console.log(newKim2);
+// **************************
 
 function deepCopyObject(obj) {
-  if (typeof obj != 'object') {
-    return;
-  }
-
   let copyObj;
   if (Array.isArray(obj)) {
     copyObj = [];
-    for (let k of obj) {
-      const tmpObj = k;
-      console.log(k, tmpObj, typeof tmpObj);
-      if (typeof tmpObj === 'object' || typeof tmpObj === 'function') {
-        copyObj.push(deepCopyObject(tmpObj));
-      } else {
-        copyObj.push(tmpObj);
-      }
-    }
   } else {
     copyObj = {};
-    for (let k in obj) {
-      const tmpObj = obj[k];
-      console.log(k, tmpObj, typeof tmpObj);
-      if (typeof tmpObj === 'object' || typeof tmpObj === 'function') {
-        copyObj[k] = deepCopyObject(tmpObj);
-      } else {
-        copyObj[k] = obj[tmpObj];
-      }
+  }
+  for (let k in obj) {
+    const tmpObj = obj[k];
+    if (typeof tmpObj === 'object') {
+      copyObj[k] = deepCopyObject(tmpObj);
+    } else {
+      copyObj[k] = tmpObj;
     }
   }
 
