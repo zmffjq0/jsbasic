a93cdc641bb5f8c37e45571efe17af302f3441aa
taeyun <zmffjq0@naver.com>
2022-08-08 15:43:47 +0900

    update

--- trythisSplice.js
+++ trythisSplice.js
@@ -7,16 +7,13 @@ function splice(arr, start, cnt = arr.length, ...args) {
     cnt === 0
       ? [...arr]
       : arr.filter((_, idx) => !(start <= idx && idx <= start + cnt - 1));
-
   if (args.length !== 0) {
     const len = res.length;
-    for (let i = start; i < len; i++) {
-      res[i + args.length] = res[i];
+    for (let i = len - 1; i >= start; i--) {
+      res[i + args.length - 1] = res[i];
     }
     res[start] = args;
-    console.log(res);
   }
-
   return res.flat(Infinity);
 }
 // console.log(a5);
@@ -26,7 +23,8 @@ const a1 = splice(arr, 1, 3); // a1 = [1, 5]
 const a2 = splice(a1, 1, 0, 2, 3, 4); // a2 = [1, 2, 3, 4, 5]
 const a3 = splice(a2, 2); // a3 = [1, 2]
 const a4 = splice(a3, 1, 1, 3, 4, 5); // a4 = [1, 3, 4, 5]
-const a5 = splice(a4, 2, 2, 7, 8, 9);
+const a5 = splice(a4, 2, 2, 7, 8, 9); // a5 = [1, 3, 7, 8, 9]
+console.log(splice([1, 2, 5, 7, 8], 2, 0, 3, 4));
 
 console.log(a1);
 console.log(a2);
